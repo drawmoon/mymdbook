5789e5654d7382e1e35303ec55d6d37a5cf1a785
drsh <1340260725@qq.com>
2021-04-12 17:02:16 +0800

    :clown_face: 提交 Minio 的示例代码

---
+++ examples/AspNetCore/MinioDemo/MinioDemo/MinioService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+using Minio;
+
+namespace MinioDemo
+{
+    public class MinioService
+    {
+        private readonly MinioClient _minioClient;
+        
+        public MinioService(string endpoint, string accessKey, string secretKey, bool useSSL = false)
+        {
+            _minioClient = new MinioClient(endpoint, accessKey, secretKey);
+            if (useSSL)
+            {
+                _minioClient.WithSSL();
+            }
+        }
+
+        public async ValueTask<T> GetAsync<T>(string key)
+        {
+            T result = default;
+            var (bucketName, objectName) = GetObjectArgs(key);
+            await _minioClient.GetObjectAsync(bucketName, objectName, async stream =>
+            {
+                DataContractJsonSerializer serializer = new(typeof(T));
+                result = (T)serializer.ReadObject(stream);
+            });
+            Debug.Assert(result != null);
+            return result;
+        }
+
+        public T Get<T>(string key) => GetAsync<T>(key).AsTask().ConfigureAwait(false).GetAwaiter().GetResult();
+        
+        public async ValueTask<string> GetAsync(string key, string fileName)
+        {
+            var (bucketName, objectName) = GetObjectArgs(key);
+            await _minioClient.GetObjectAsync(bucketName, objectName, fileName);
+            return fileName;
+        }
+
+        public string Get(string key, string fileName) =>
+            GetAsync(key, fileName).AsTask().ConfigureAwait(false).GetAwaiter().GetResult();
+        
+        public async ValueTask SetAsync(string key, object value)
+        {
+            var (bucketName, objectName) = GetObjectArgs(key);
+            await using MemoryStream memoryStream = new();
+            DataContractJsonSerializer serializer = new(value.GetType());
+            serializer.WriteObject(memoryStream, value);
+            memoryStream.Position = 0;
+            await memoryStream.FlushAsync();
+            await _minioClient.PutObjectAsync(bucketName, objectName, memoryStream, memoryStream.Length,
+                "application/json");
+        }
+
+        public void Set(string key, object value) =>
+            SetAsync(key, value).AsTask().ConfigureAwait(false).GetAwaiter().GetResult();
+
+        public async ValueTask<string> SetAsync(string key, string fileName)
+        {
+            var (bucketName, objectName) = GetObjectArgs(key);
+            await _minioClient.PutObjectAsync(bucketName, objectName, fileName);
+            return fileName;
+        }
+
+        public string Set(string key, string fileName) =>
+            SetAsync(key, fileName).AsTask().GetAwaiter().GetResult();
+        
+        public async ValueTask RemoveAsync(string key)
+        {
+            var (bucketName, objectName) = GetObjectArgs(key);
+            await _minioClient.RemoveObjectAsync(bucketName, objectName);
+        }
+
+        public void Remove(string key) => RemoveAsync(key).AsTask().ConfigureAwait(false).GetAwaiter().GetResult();
+
+        private static (string bucketName, string objectName) GetObjectArgs(string key)
+        {
+            var split = key.Split("&");
+            if (split.Length != 2)
+            {
+                throw new NotSupportedException("Key is not a string composed of '{bucketName}&{objectName}'");
+            }
+            return (split[0], split[1]);
+        }
+    }
+}
---
+++ examples/AspNetCore/MinioDemo/MinioDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace MinioDemo
+{
+    class Program
+    {
+        private const string Endpoint = "127.0.0.1:9000";
+        private const string AccessKey = "AKIAIOSFODNN7EXAMPLE";
+        private const string SecretKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+        
+        static void Main(string[] args)
+        {
+            try
+            {
+                MinioService minioService = new(Endpoint, AccessKey, SecretKey);
+                
+                Console.WriteLine("1. Set and Get Object");
+                // Put Object
+                minioService.Set("test&user/a1/user.json", new User
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "xl"
+                });
+                // Get Object
+                var user = minioService.Get<User>("test&user/a1/user.json");
+                Console.WriteLine($"{user.Id}  {user.Name}");
+                // Remove File
+                minioService.Remove("test&user/a1/user.json");
+
+                Console.WriteLine("2. Set and Get File");
+                var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "tmp");
+                if (!Directory.Exists(filePath))
+                {
+                    Directory.CreateDirectory(filePath);
+                }
+                var fileName = Path.Combine(filePath, "user.json");
+                File.WriteAllText(fileName, JsonSerializer.Serialize(user));
+                // Put File
+                minioService.Set("test&user/a2/user.json", fileName);
+                // Get File
+                var fileName2 = Path.Combine(filePath, "user2.json");
+                minioService.Get("test&user/a2/user.json", fileName2);
+                Console.WriteLine(File.ReadAllText(fileName2));
+                // Remove File
+                minioService.Remove("test&user/a2/user.json");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+    
+    public class User
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
