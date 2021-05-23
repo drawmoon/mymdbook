d1d11d9faa95ca5aee4d7e6677904b3af924c114
drawmoon <1340260725@qq.com>
2021-05-23 16:35:33 +0800

    :clown_face: node-html-to-word，图片宽高问题

--- examples/TypeScripts/html-to-word-demo/src/index.ts
+++ examples/TypeScripts/html-to-word-demo/src/index.ts
@@ -2,7 +2,11 @@ import { asBlob } from 'html-docx-js-typescript';
 import * as path from 'path';
 import * as fs from 'fs';
 import { JSDOM } from 'jsdom';
-// import { loadImage, createCanvas } from 'canvas';
+import { loadImage, createCanvas } from 'canvas';
+// import * as HTMLtoDOCX from 'html-to-docx';
+
+const targetWidth = 595;
+const targetHeight = 842;
 
 const htmlTemplate = `
 <!DOCTYPE html>
@@ -24,7 +28,7 @@ const htmlTemplate = `
       padding: 5px 10px;
     }
     img {
-      max-width: 1600px;
+      max-width: 100%;
       max-height: 100%;
     }
     figure.image {
@@ -40,6 +44,39 @@ const htmlTemplate = `
 </body>
 </html>`;
 
+function imgZoom(imgWidth: number, imgHeight: number) {
+  let width: number;
+  let height: number;
+
+  if (imgWidth > 0 && imgHeight > 0) {
+    //原图片宽高比例 大于 指定的宽高比例，这就说明了原图片的宽度必然 > 高度
+    if (imgWidth / imgHeight >= targetWidth / targetHeight) {
+      if (imgWidth > targetWidth) {
+        width = targetWidth;
+        // 按原图片的比例进行缩放
+        height = (imgHeight * targetWidth) / imgWidth;
+      } else {
+        // 按照图片的大小进行缩放
+        width = imgWidth;
+        height = imgHeight;
+      }
+    } else {
+      // 原图片的高度必然 > 宽度
+      if (imgHeight > targetHeight) {
+        height = targetHeight;
+        // 按原图片的比例进行缩放
+        width = (imgWidth * targetHeight) / imgHeight;
+      } else {
+        // 按原图片的大小进行缩放
+        width = imgWidth;
+        height = imgHeight;
+      }
+    }
+  }
+
+  return { width, height };
+}
+
 async function saveDocx(): Promise<void> {
   const htmlString = fs.readFileSync(
     path.join(process.cwd(), 'assets', 'content.html'),
@@ -51,45 +88,83 @@ async function saveDocx(): Promise<void> {
   const dom = new JSDOM(htmlString);
   const doc = dom.window.document;
 
-  // const canvas = doc.createElement('canvas');
-  // const ctx = canvas.getContext('2d');
+  console.log('Converting image to base64');
 
-  // 将图片转换为 base64
   for (const img of doc.querySelectorAll('img')) {
     const imgPath = path.join(process.cwd(), img.src);
 
-    const imgBase64 = fs.readFileSync(imgPath, { encoding: 'base64' });
-    img.src = `data:image/png;base64,${imgBase64}`;
+    const svg = await loadImage(imgPath);
+    console.log(`The image is width: ${svg.width} height: ${svg.height}`);
 
-    // img.src = imgPath;
-    // // preparing canvas for drawing
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // canvas.width = img.width;
-    // canvas.height = img.height;
+    if (svg.width > targetWidth || svg.height > targetHeight) {
+      const { width, height } = imgZoom(svg.width, svg.height);
 
-    // ctx.drawImage(img, 0, 0);
-    // // by default toDataURL() produces png image, but you can also export to jpeg
-    // // checkout function's documentation for more details
-    // const dataURL = canvas.toDataURL();
-    // // img.setAttribute('src', dataURL);
-    // img.src = dataURL;
+      console.log(
+        `The image exceeds the specified width and height. Being zoomed, width: ${width}, height: ${height}`,
+      );
 
-    // const imgBuffer = fs.readFileSync(imgPath);
-    // const svg = await loadImage(imgBuffer);
-    // // const canvas = createCanvas(img.width, img.height);
-    // const canvas = createCanvas(500, 500);
-    // const context = canvas.getContext('2d');
-    // context.drawImage(svg, 0, 0);
-    // img.src = canvas.toDataURL('image/png');
+      const canvas = createCanvas(width, height);
+      const context = canvas.getContext('2d');
+      context.drawImage(svg, 0, 0, width, height);
+
+      img.src = canvas.toDataURL('image/png');
+    } else {
+      const imgBase64 = fs.readFileSync(imgPath, { encoding: 'base64' });
+      img.src = `data:image/png;base64,${imgBase64}`;
+    }
   }
-
-  // canvas.remove();
 
   const html = htmlTemplate.replace('{htmlContent}', doc.body.innerHTML);
 
-  const data = await asBlob(html, { orientation: 'landscape' });
+  const data = await asBlob(html);
   const filename = path.join(process.cwd(), 'dist', 'file.docx');
   fs.writeFileSync(filename, data as Buffer);
 }
+
+// async function saveDocx(): Promise<void> {
+//   const htmlString = fs.readFileSync(
+//     path.join(process.cwd(), 'assets', 'content.html'),
+//     {
+//       encoding: 'utf-8',
+//     },
+//   );
+
+//   const dom = new JSDOM(htmlString);
+//   const doc = dom.window.document;
+
+//   console.log('Converting image to base64');
+
+//   for (const img of doc.querySelectorAll('img')) {
+//     const imgPath = path.join(process.cwd(), img.src);
+
+//     const svg = await loadImage(imgPath);
+//     console.log(`The image is width: ${svg.width} height: ${svg.height}`);
+
+//     if (svg.width > targetWidth || svg.height > targetHeight) {
+//       const { width, height } = imgZoom(svg.width, svg.height);
+
+//       console.log(
+//         `The image exceeds the specified width and height. Being zoomed, width: ${width}, height: ${height}`,
+//       );
+
+//       const canvas = createCanvas(width, height);
+//       const context = canvas.getContext('2d');
+//       context.drawImage(svg, 0, 0, width, height);
+
+//       img.src = canvas.toDataURL('image/png');
+//     } else {
+//       const imgBase64 = fs.readFileSync(imgPath, { encoding: 'base64' });
+//       img.src = `data:image/png;base64,${imgBase64}`;
+//     }
+//   }
+
+//   const html = htmlTemplate.replace('{htmlContent}', doc.body.innerHTML);
+
+//   const data = await HTMLtoDOCX(html, null, {
+//     table: { row: { cantSplit: true } },
+//   });
+//   const filename = path.join(process.cwd(), 'dist', 'file.docx');
+//   fs.writeFileSync(filename, data as Buffer);
+// }
 
 saveDocx().then();
