2d6da155b999fc5759dc1e4753bc67a1e2ef8613
drawmoon <1340260725@qq.com>
2021-08-11 13:54:31 +0800

    python > fn **dict

--- examples/PythonExamples/fn.py
+++ examples/PythonExamples/fn.py
@@ -1,4 +1,19 @@
+from typing import Dict
+
+
 def print_msg(msg: str) -> None:
   print(msg)
 
 print_msg("Hello world!")
+
+
+
+# Dict
+def print_dict(d: Dict, ner, **args):
+  print("The value of parameter d is:", d)
+  print("The value of parameter ner is:", ner)
+  print("The value of parameter args is:", args)
+
+
+some_dict = {"t": "fever", "ner": "cold"}
+print_dict(some_dict, **some_dict)
