25e39f9c133903ae29d2f7c08a49253c273019de
drawmoon <1340260725@qq.com>
2021-08-24 11:36:12 +0800

    获取本月的最后一天，本周第一天与本周最后一天

--- examples/PythonExamples/date_time.py
+++ examples/PythonExamples/date_time.py
@@ -1,6 +1,7 @@
 from datetime import datetime, date, timedelta
 from dateutil.relativedelta import relativedelta
 from dateutil.parser import parse
+import calendar
 
 # 获取当前时间
 now = datetime.now()
@@ -35,3 +36,11 @@ print(now + relativedelta(years=1))
 # 减一年
 print(now - relativedelta(years=1))
 print(now + relativedelta(years=-1))
+
+# 本月最后一天
+print(calendar.monthrange(now.year, now.month))
+
+# 本周第一天
+print(now - timedelta(days=now.weekday()))
+# 本周最后一天
+print(now + timedelta(days=6 - now.weekday()))
