83396a05076ace16c4e10d8d63e263f7ea146611
drawmoon <1340260725@qq.com>
2021-09-10 18:27:23 +0800

    正则

--- examples/PythonExamples/regex.py
+++ examples/PythonExamples/regex.py
@@ -1,9 +1,20 @@
 import re
 
-pattern = "^[前后]?(?P<digit>[0-9零一二两三四五六七八九十])个?(?:[年月日]|星期|季度)以?[前后]?$"
+str_pattern = r"^[前后]?(?P<digit>[0-9零一二两三四五六七八九十])个?(?:[年月日]|星期|季度)以?[前后]?$"
 
-match = re.search(pattern, "前两个星期")
-
+match = re.search(str_pattern, "前两个星期")
 if match is not None:
     digit = match.group("digit")
     print(digit)
+
+# pattern
+pattern = re.compile(str_pattern)
+match = pattern.search("前两个星期")
+if match is not None:
+    digit = match.group("digit")
+    print(digit)
+
+# 不区分大小写
+pattern = re.compile(r"(a)", re.I)
+match = pattern.search("ABC")
+print(match.group(1))
