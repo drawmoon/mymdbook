a2c803984c47e636ea701e758bca678d50dc2609
drawmoon <1340260725@qq.com>
2021-09-14 18:34:40 +0800

    函数的可变长参数

--- examples/PythonExamples/fn.py
+++ examples/PythonExamples/fn.py
@@ -1,6 +1,7 @@
 from typing import Dict
 
 
+# 函数声明与调用
 def print_msg(msg: str) -> None:
     print(msg)
 
@@ -8,12 +9,34 @@ def print_msg(msg: str) -> None:
 print_msg("Hello world!")
 
 
-# Dict
-def print_dict(d: Dict, ner, **args):
+# 可变长参数 *
+def some_params_fn(s: str, *args):
+    print(s)
+    print(len(args))
+    for arg in args:
+        print(arg)
+
+
+some_params_fn('hello', 1, 2, 3)
+
+
+def some_fn(s: str, n: int):
+    print(s, n)
+
+
+def some_params_fn2(*args):
+    some_fn(*args)
+
+
+some_params_fn2('hello', 123)
+
+
+# 可变长参数 **
+def some_params_fn3(d: Dict, ner: str, **args):
     print("The value of parameter d is:", d)
     print("The value of parameter ner is:", ner)
     print("The value of parameter args is:", args)
 
 
 some_dict = {"t": "fever", "ner": "cold"}
-print_dict(some_dict, **some_dict)
+some_params_fn3(some_dict, **some_dict)
