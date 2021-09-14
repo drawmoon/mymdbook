3f9f8e519d6799a63b3f7f42bcbb0ce89af0add7
drawmoon <1340260725@qq.com>
2021-09-14 18:28:31 +0800

    :art:

--- examples/PythonExamples/regex.py
+++ examples/PythonExamples/regex.py
@@ -1,37 +1,37 @@
 import re
 
 
-str_pattern = r"^[前后]?(?P<digit>[0-9零一二两三四五六七八九十])个?(?:[年月日]|星期|季度)以?[前后]?$"
+rq_reg_exp = r"^(?P<prefix>[前后])?(?P<digit>[0-9零一二两三四五六七八九十])个?(?:[年月周日天]|星期|季度)以?(?P<suffix>[前后内])?$"
 
-match = re.search(str_pattern, "前两个星期")
-if match is not None:
-    digit = match.group("digit")
+result = re.search(rq_reg_exp, "前两个星期")
+if result is not None:
+    digit = result.group("digit")
     print(digit)
 
 # pattern
-pattern = re.compile(str_pattern)
-match = pattern.search("前两个星期")
-if match is not None:
-    digit = match.group("digit")
+rq_pattern = re.compile(rq_reg_exp)
+result = rq_pattern.search("前两个星期")
+if result is not None:
+    digit = result.group("digit")
     print(digit)
 
 
 # 不区分大小写
-pattern = re.compile(r"(a)", re.I)
-match = pattern.search("ABC")
-print(match.group(1))
+str_pattern = re.compile(r"(a)", re.I)
+result = str_pattern.search("ABC")
+print(result.group(1))
 
 
 # 将匹配到的值替换为新的值
-pattern = re.compile(r"(B)")
-print(pattern.sub("1", "ABC"))
+str_pattern = re.compile(r"(B)")
+print(str_pattern.sub("1", "ABC"))
 
-pattern = re.compile(r"\*{3}(\S+)\*{3}")
-print(pattern.sub(r"---\1---", "***ABC***"))
+str_pattern = re.compile(r"\*{3}(\S+)\*{3}")
+print(str_pattern.sub(r"---\1---", "***ABC***"))
 
 
 # 将匹配到的分组的值替换为新的值
 input_str = """<figure class=3D"image"><img src=3D"file:///C:/fake/image0.png"> <figcaption>3D图形</figcaption>"""
-pattern = re.compile(r"<[^>]*=(?P<stain>3D)+\"\S+\"\s*\/?>")
-repl_str = pattern.sub(lambda x: input_str[x.start():x.end()].replace(x.group("stain"), ""), input_str)
+htm_pattern = re.compile(r"<[^>]*=(?P<stain>3D)+\"\S+\"\s*\/?>")
+repl_str = htm_pattern.sub(lambda x: input_str[x.start():x.end()].replace(x.group("stain"), ""), input_str)
 print(repl_str)
